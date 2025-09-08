d8eabdc191723152d792cc96b8162863eeff68a0
Joel Koen <mail@joelkoen.com>
2025-09-08 12:27:26 +1000

    feat: getting offset in proxy

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            pkgs.rust-analyzer-nightly
+          ];
+        };
+      });
+}
---
+++ src/offset.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, Clone, Copy)]
+pub struct ChunkOffset {
+    pub x: i32,
+    pub z: i32,
+}
